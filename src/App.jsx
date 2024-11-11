387272ae6e7435809a50cf2f52615d0c0bf67d3d
21subham <subhamrajbhandari@gmail.com>
2024-11-11 21:03:37 +0545

    deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { InputBox } from "./components";
 import useCurrencyInfo from "./hooks/useCurrencyInfo";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // Added routes for routing
 
 function App() {
   const [amount, setAmount] = useState(0);
   const [from, setFrom] = useState("USD");
-  const [to, setTo] = useState("INR");
+  const [to, setTo] = useState("NPR");
   const [convertedAmount, setConvertedAmount] = useState(0);
 
   const { data: currencyInfo, loading, error } = useCurrencyInfo(from);
@@ -30,71 +31,81 @@ function App() {
   };
 
   return (
-    <div
-      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
-      style={{
-        backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
-      }}
-    >
-      <div className="w-full">
-        <div className="w-full max-w-md mx-auto border border-gray-600 rounded-lg p-5 backdrop-blur-sm bg-white/30">
-          <form onSubmit={handleSubmit}>
-            {/* From Currency Input */}
-            <div className="w-full mb-1">
-              <InputBox
-                label="From"
-                amount={amount}
-                currencyOptions={options}
-                onCurrencyChange={(currency) => setFrom(currency)}
-                selectCurrency={from}
-                onAmountChange={(amount) => setAmount(amount)}
-              />
-            </div>
-
-            {/* Swap Button */}
-            <div className="relative w-full h-0.5">
-              <button
-                type="button"
-                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
-                onClick={swap}
-              >
-                Swap
-              </button>
-            </div>
-
-            {/* To Currency Input */}
-            <div className="w-full mt-1 mb-4">
-              <InputBox
-                label="To"
-                amount={convertedAmount}
-                currencyOptions={options}
-                onCurrencyChange={(currency) => setTo(currency)}
-                selectCurrency={to}
-                amountDisable={true}
-              />
-            </div>
-
-            {/* Convert Button */}
-            <button
-              type="submit"
-              className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
-              disabled={loading || amount <= 0 || options.length === 0} // Disable if loading or amount is 0
+    <BrowserRouter>
+      <Routes>
+        {/* Add Routes component */}
+        <Route
+          path="/"
+          element={
+            <div
+              className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+              style={{
+                backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
+              }}
             >
-              {loading
-                ? "Loading..."
-                : `Convert ${from.toUpperCase()} to ${to.toUpperCase()}`}
-            </button>
-          </form>
+              <div className="w-full">
+                <div className="w-full max-w-md mx-auto border border-gray-600 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+                  <form onSubmit={handleSubmit}>
+                    {/* From Currency Input */}
+                    <div className="w-full mb-1">
+                      <InputBox
+                        label="From"
+                        amount={amount}
+                        currencyOptions={options}
+                        onCurrencyChange={(currency) => setFrom(currency)}
+                        selectCurrency={from}
+                        onAmountChange={(amount) => setAmount(amount)}
+                      />
+                    </div>
 
-          {/* Display Error Message */}
-          {error && (
-            <div className="mt-4 text-red-600 text-center">
-              <p>{error}</p>
+                    {/* Swap Button */}
+                    <div className="relative w-full h-0.5">
+                      <button
+                        type="button"
+                        className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                        onClick={swap}
+                      >
+                        Swap
+                      </button>
+                    </div>
+
+                    {/* To Currency Input */}
+                    <div className="w-full mt-1 mb-4">
+                      <InputBox
+                        label="To"
+                        amount={convertedAmount}
+                        currencyOptions={options}
+                        onCurrencyChange={(currency) => setTo(currency)}
+                        selectCurrency={to}
+                        amountDisable={true}
+                      />
+                    </div>
+
+                    {/* Convert Button */}
+                    <button
+                      type="submit"
+                      className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
+                      disabled={loading || amount <= 0 || options.length === 0} // Disable if loading or amount is 0
+                    >
+                      {loading
+                        ? "Loading..."
+                        : `Convert ${from.toUpperCase()} to ${to.toUpperCase()}`}
+                    </button>
+                  </form>
+
+                  {/* Display Error Message */}
+                  {error && (
+                    <div className="mt-4 text-red-600 text-center">
+                      <p>{error}</p>
+                    </div>
+                  )}
+                </div>
+              </div>
             </div>
-          )}
-        </div>
-      </div>
-    </div>
+          }
+        />{" "}
+      </Routes>
+    </BrowserRouter>
   );
 }
 

b822c10e5b65b7099346117d99e6aed4b7cb3dd3
21subham <subhamrajbhandari@gmail.com>
2024-11-12 13:17:18 +0545

    final

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { InputBox } from "./components";
 import useCurrencyInfo from "./hooks/useCurrencyInfo";
 import { BrowserRouter } from "react-router-dom"; // Added routes for routing
-
+ 
 function App() {
   const [amount, setAmount] = useState(0);
   const [from, setFrom] = useState("USD");
